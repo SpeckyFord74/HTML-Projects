b32b24f3ec98498224f0aefe1d7769a83932f141
Corey <107110478+SpeckyFord74@users.noreply.github.com>
2024-11-05 15:09:40 +0000

    Add files via upload

---
+++ Movie App/script.js
@@ -0,0 +1,79 @@
+const API_URL =
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=2f0e9debacd8c5545192e55649d66949&query=";
+const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
+const SEARCH_API =
+  'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="';
+const TMBD_URL = "https://www.themoviedb.org/movie/";
+
+const main = document.getElementById("main");
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+
+getMovies(API_URL);
+
+async function getMovies(url) {
+  const res = await fetch(url);
+  const data = await res.json();
+
+  console.log(data.results);
+  showMovies(data.results);
+}
+
+function showMovies(movies) {
+  main.innerHTML = "";
+
+  movies.forEach((movie) => {
+    const { title, poster_path, vote_average, overview, id } = movie;
+
+    const movieE1 = document.createElement("div");
+    movieE1.classList.add("movie");
+
+    movieE1.innerHTML = `
+            <img src="${IMG_PATH + poster_path}" alt="${title}">
+            <div class="movie-info">
+                <h3>${title}</h3>
+                <span class="${getClassByRate(
+                  vote_average
+                )}">${vote_average}</span>
+            </div>
+
+            <div class="overview">
+                <h3>Overview</h3>
+                ${overview}
+                <br>
+                <a href="${TMBD_URL}${id}" target="_blank">View More</a>
+            </div>
+        `;
+    main.appendChild(movieE1);
+  });
+}
+
+function getClassByRate(vote) {
+  if (vote >= 8) {
+    return "green";
+  } else if (vote >= 5) {
+    return "orange";
+  } else {
+    return "red";
+  }
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log("Search Submitted");
+
+  const searchTerm = search.value;
+
+  if (searchTerm && searchTerm !== "") {
+    getMovies(SEARCH_API + searchTerm);
+
+    search.value = "";
+  } else {
+    window.location.reload();
+  }
+});
+
+function returnHome() {
+  getMovies(API_URL);
+  search.value = "";
+}
--- Weather App/script.js
+++ Weather App/script.js
@@ -1,38 +1,28 @@
-// API Key for OpenWeatherMap API
-const apiKey = "Your API Key";
-
-// Base URL for OpenWeatherMap API
+const apiKey = "269114a7a27a95530325d6af01e1884a";
 const url = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
-// Selectors for search box, search button, and weather icon
 const searchBox = document.querySelector(".search input");
 const searchButton = document.querySelector(".search button");
+
 const weatherIcon = document.querySelector(".weather-icon");
 
-// Function to check the weather for a given city
 async function checkWeather(city) {
-  // Construct the API request URL
   const response = await fetch(url + city + `&appid=${apiKey}`);
 
-  // Check if the response was successful
   if (response.status == 404) {
-    // Display error message if city not found
     document.querySelector(".error").style.display = "block";
     document.querySelector(".weather").style.display = "none";
   } else {
-    // Parse the JSON response
     var data = await response.json();
 
-    // Log the response data for debugging
     console.log(data);
 
-    // Update the UI with the weather data
     document.querySelector(".temp").innerHTML = Math.round(data.main.temp);
     document.querySelector(".city").innerHTML = data.name;
+
     document.querySelector(".humidity").innerHTML = data.main.humidity;
     document.querySelector(".wind").innerHTML = Math.round(data.wind.speed);
 
-    // Update the weather icon based on the weather condition
     if (data.weather[0].main == "Clouds") {
       weatherIcon.src = "images/cloud.png";
     } else if (data.weather[0].main == "Rain") {
@@ -43,14 +33,11 @@ async function checkWeather(city) {
       weatherIcon.src = "images/Foggy.png";
     }
 
-    // Hide error message and display weather data
     document.querySelector(".error").style.display = "none";
     document.querySelector(".weather").style.display = "block";
   }
 }
 
-// Add event listener to search button
 searchButton.addEventListener("click", () => {
-  // Call the checkWeather function with the search box value
   checkWeather(searchBox.value);
 });
